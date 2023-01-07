9a7995147636d3feb9631302126cdae187d25352
PaladinKnightMaster <paladinknight.master@gmail.com>
2023-01-06 22:22:27 -0500

    refactored and separated code into functions

--- script.js
+++ script.js
@@ -1,59 +1,46 @@
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const totalScore = document.getElementById("total-score");
+const userChoice = document.getElementById("user-play");
+const computerChoice = document.getElementById("computer-play");
+const whoWon = document.getElementById("winner");
+rock.addEventListener("click", (e) => playGame(e));
+paper.addEventListener("click", (e) => playGame(e));
+scissors.addEventListener("click", (e) => playGame(e));
 
 let userCount = 0;
 let computerCount = 0;
-const results = ['rock', 'paper', 'scissor'];
 
-
-document.getElementById('rock').addEventListener("click", (e) => playGame(e));
-document.getElementById('paper').addEventListener("click", (e) => playGame(e));
-document.getElementById('scissor').addEventListener("click", (e) => playGame(e));
-
-let totalScore = document.getElementById("total-score");
-let userChoice = document.getElementById("user-play");
-let computerChoice = document.getElementById("computer-play");
-let whoWon = document.getElementById("winner");
-
+const results = ['rock', 'paper', 'scissors'];
 
 function playGame(input) {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const computerPlay = results[randomNumber];
+    const userPlay = input.target.id;
+    const winner = getWinner(computerPlay, userPlay);
 
-    let randomNumber = Math.floor(Math.random() * 3);
-    let computerPlay = results[randomNumber];
-    let userPlay = input.target.id;
+    if (winner !== "It's a Tie!") updateScores(winner);
+
     userChoice.textContent = `The User chose: ${userPlay}`;
     computerChoice.textContent = `The Computer chose: ${computerPlay}`;
+    totalScore.textContent = `USER: ${userCount} - COMPUTER: ${computerCount}`;
+    whoWon.textContent = winner;
+}
 
-
-    if (computerPlay === userPlay) {
-        console.log('It is a Tie');
-        whoWon.textContent = "It's a Tie!";
-    } else {
-        if (userPlay === 'rock' && computerPlay === 'paper') {
-            computerCount += 1;
-            whoWon.textContent = "Computer Wins!";
-
-        } else if (userPlay === 'rock' && computerPlay === 'scissor') {
-            userCount += 1;
-            whoWon.textContent = "User Wins!";
-
-        } else if (userPlay === 'scissor' && computerPlay === 'paper') {
-            userCount += 1;
-            whoWon.textContent = "User Wins!";
-
-        } else if (userPlay === 'scissor' && computerPlay === 'rock') {
-            computerCount += 1;
-            whoWon.textContent = "Computer Wins!";
-
-        } else if (userPlay === 'paper' && computerPlay === 'rock') {
-            userCount += 1;
-            whoWon.textContent = "User Wins!";
-
-        } else if (userPlay === 'paper' && computerPlay === 'scissor') {
-            computerCount += 1;
-            whoWon.textContent = "Computer Wins!";
-
-        }
-
-        totalScore.textContent = `USER: ${userCount} - COMPUTER: ${computerCount}`;
-
+function getWinner(computerChoice, userChoice) {
+    if (computerChoice === userChoice) return "It's a Tie!";
+    switch (userChoice) {
+        case 'rock':
+            return computerChoice === 'paper' ? 'Computer Wins!' : 'User Wins!';
+        case 'paper':
+            return computerChoice === 'scissors' ? 'Computer Wins!' : 'User Wins!';
+        case 'scissors':
+            return computerChoice === 'rock' ? 'Computer Wins!' : 'User Wins!';
     }
-}    
+}
+
+function updateScores(winner) {
+    if (winner === 'User Wins!') userCount++;
+    if (winner === 'Computer Wins!') computerCount++;
+}
